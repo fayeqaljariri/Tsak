fbb591aa197d6f36a0a96309da9fad04d61fde74
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-17 00:58:50 +0300

    add element to navbar

--- my-react-app/src/component/navbar.js
+++ my-react-app/src/component/navbar.js
@@ -4,8 +4,29 @@ import "./navbar.css"
 const Navigation = () => {
   return (
     <>
-      <div className="NavBar">
+    <div class="topnav">
+    <button className="logout">Logout</button>
+    <Link className="Link" to="/FAQs">
+          FAQs
+          </Link> 
+          <Link className="Link" to="/Mrketplace">
+            Mrketplace
+          </Link>
+          <Link className="Link" to="/Leaderboards">
+            Leaderboards
+          </Link>
+  
+          <Link className="Link" to="/Order">
+            Order
+          </Link>
+          <Link className="Link" to="/Home">
+           Home
+          </Link>
+          <img className="logo" src="\Twenteefore - Logo 2 (2).png"></img>
          
+</div>
+      {/* <div className="NavBar">
+      
           <Link className="Link" to="/FAQs">
           FAQs
           </Link> 
@@ -23,7 +44,7 @@ const Navigation = () => {
           </Link>
           <button className="logout">Logout</button>
         
-      </div>
+      </div> */}
     </>
   );
 };
