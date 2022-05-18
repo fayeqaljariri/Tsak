8b9a89475ed42a1188e4976c151e7eac1a0f9187
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-18 15:19:27 +0300

    REMOVE unused code

--- my-react-app/src/component/FAQs.js
+++ my-react-app/src/component/FAQs.js
@@ -1,13 +1,9 @@
 import React ,{useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { UncontrolledAccordion,AccordionItem,AccordionHeader,AccordionBody, } from 'reactstrap';
+
 import  "./FAQs.css";
 const Faqs =()=>{
-const questions=[{question:"lorem?",answer:"jscc wslii wshcd ieedc jcfwd wcclo edkkec nojc"},{question:"hdv ldvn dlsvn",answer:"dvcl dsv sxdvdxv dv d;mjm peocd ecde"}]
-const [show, setShow] = useState(false);
-const [show2, setShow2] = useState(false);
-const [show3, setShow3] = useState(false);
-const [show4, setShow4] = useState(false);
-const [show5, setShow5] = useState(false);
-
 
 return(
     <>
@@ -21,42 +17,51 @@ return(
     <br></br>
     <br></br>
     <br></br>
-    <hr></hr>
-    <div className="question">
-            <h2>lorem?</h2>
-            {!show ? <p className="show" onClick={()=>{setShow(true)}}>+</p>:<p className="show" onClick={()=>{setShow(false)}}>x</p>}
-        </div><div>
-        {show &&<p className="answer">"jscc wslii wshcd ieedc jcfwd wcclo edkkec nojc"</p>} 
-<hr></hr>
-    </div>
-    <div className="question">
-            <h2>dqd bshswln swcnm?</h2>
-            {!show2 ? <p className="show" onClick={()=>{setShow2(true)}}>+</p>:<p className="show" onClick={()=>{setShow2(false)}}>x</p>}
-        </div><div>
-        {show2 &&<p className="answer">ksqs mwdei qadb </p>} 
-<hr></hr>
-    </div>
-    <div className="question">
-            <h2>slwdn rew pkws ?</h2>
-            {!show3 ? <p className="show" onClick={()=>{setShow3(true)}}>+</p>:<p className="show" onClick={()=>{setShow3(false)}}>x</p>}
-        </div><div>
-        {show3 &&<p className="answer">hdv ldvn dlsvn</p>} 
-<hr></hr>
-    </div>
-    <div className="question">
-            <h2>jkls fkop wdkd ?</h2>
-            {!show4 ? <p className="show" onClick={()=>{setShow4(true)}}>+</p>:<p className="show" onClick={()=>{setShow4(false)}}>x</p>}
-        </div><div>
-        {show4 &&<p className="answer">lorem ipsum swefe ssssds wdsp </p>}
-<hr></hr>
-    </div>
-    <div className="question">
-            <h2>lorem ipsum swefe sss?</h2>
-            {!show5 ? <p className="show" onClick={()=>{setShow5(true)}}>+</p>:<p className="show" onClick={()=>{setShow5(false)}}>x</p>}
-        </div><div>
-        {show5 &&<p className="answer">hdv ldvn dlsvn</p>} 
-<hr></hr>
-    </div>
+    
+<div>
+  <UncontrolledAccordion defaultOpen="1">
+    <AccordionItem>
+      <AccordionHeader targetId="1">
+          <strong>Lorem ?</strong>
+        
+      </AccordionHeader>
+      <AccordionBody accordionId="1">     
+     This is the first item's accordion body.  You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the
+    .accordion-body, though the transition does limit overflow.
+      </AccordionBody>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionHeader targetId="2">
+      <strong>Lorem hsidhs ?</strong>
+      </AccordionHeader>
+      <AccordionBody accordionId="2">
+        
+          This is the second item's accordion body.
+       
+        You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the{' '}
+        
+          .accordion-body
+        
+        , though the transition does limit overflow.
+      </AccordionBody>
+    </AccordionItem>
+    <AccordionItem>
+      <AccordionHeader targetId="3">
+      <strong>Lorem hsidhs ?</strong>
+      </AccordionHeader>
+      <AccordionBody accordionId="3">
+        
+          This is the third item's accordion body.
+        
+        You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the{' '}
+        
+          .accordion-body
+        
+        , though the transition does limit overflow.
+      </AccordionBody>
+    </AccordionItem>
+  </UncontrolledAccordion>
+</div>
     </div>
  
     
--- my-react-app/src/component/navbar.js
+++ my-react-app/src/component/navbar.js
@@ -29,26 +29,6 @@ const Navigation = () => {
           </nav>
 
 </div>
-      {/* <div className="NavBar">
-      
-          <Link className="Link" to="/FAQs">
-          FAQs
-          </Link> 
-          <Link className="Link" to="/Order">
-            Order
-          </Link>
-          <Link className="Link" to="/Leaderboards">
-            Leaderboards
-          </Link>
-          <Link className="Link" to="/Mrketplace">
-            Mrketplace
-          </Link>
-          <Link className="Link" to="/FAQs">
-          FAQs
-          </Link>
-          <button className="logout">Logout</button>
-        
-      </div> */}
     </>
   );
 };
