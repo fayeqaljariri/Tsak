bfcce3fb85d2fb90ba37b07eed19e957bc036cbc
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-19 12:40:25 +0300

    make ajason file for link

---
+++ my-react-app/src/Data/navlink.js
@@ -0,0 +1,7 @@
+export const data = [
+  { path: "/FAQs", name: "FAQs" },
+  { path: "/Mrketplace", name: "Mrketplace" },
+  { path: "/Leaderboards", name: "Leaderboards" },
+  { path: "/Order", name: "Order" },
+  { path: "/Home", name: "Home" },
+];
