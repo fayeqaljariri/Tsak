47141ed526c28fe039b756c96658e8d21949ce92
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-19 11:08:59 +0300

    add new file for faqs pages

---
+++ my-react-app/src/pages/FAQ/FAQs.js
@@ -0,0 +1,28 @@
+import React ,{useState} from "react";
+import Accordion from "../../component/Accordion/Accordion";
+import  "./FAQs.css";
+const Faqs =()=>{
+
+return(
+    <>
+     
+    <div className="header">
+        <div className="head">
+    <h1 >Frequently Asked Questions</h1>
+    <p className="smallheader">Feel free to send us your qustions or comments below</p>
+    </div>
+    <br></br>
+    <br></br>
+    <br></br>
+    <br></br>
+    <Accordion></Accordion>
+    </div>
+ 
+    
+    
+    </>
+)
+
+
+}
+export default Faqs;
