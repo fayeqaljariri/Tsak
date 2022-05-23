4fd7e4f52a0796ff6aff239b50f3b12023286860
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-23 15:54:02 +0300

    make map for data

--- my-react-app/src/component/Accordion/Accordion.js
+++ my-react-app/src/component/Accordion/Accordion.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Accordion.css"
+import "./Accordion.css";
 import {
   UncontrolledAccordion,
   AccordionItem,
@@ -7,43 +7,25 @@ import {
   AccordionBody,
 } from "reactstrap";
 import React from "react";
+import { Data } from "./data";
 
 const Accordion = () => {
   return (
     <div className="qustion">
       <UncontrolledAccordion defaultOpen="1">
         <AccordionItem>
-          <AccordionHeader targetId="1">
-            <strong>Lorem ?</strong>
-          </AccordionHeader>
-          <AccordionBody accordionId="1">
-            This is the first item's accordion body. You can modify any of this
-            with custom CSS or overriding our default variables. It's also worth
-            noting that just about any HTML can go within the .accordion-body,
-            though the transition does limit overflow.
-          </AccordionBody>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionHeader targetId="2">
-            <strong>Lorem hsidhs ?</strong>
-          </AccordionHeader>
-          <AccordionBody accordionId="2">
-            This is the second item's accordion body. You can modify any of this
-            with custom CSS or overriding our default variables. It's also worth
-            noting that just about any HTML can go within the .accordion-body ,
-            though the transition does limit overflow.
-          </AccordionBody>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionHeader targetId="3">
-            <strong>Lorem hsidhs ?</strong>
-          </AccordionHeader>
-          <AccordionBody accordionId="3">
-            This is the third item's accordion body. You can modify any of this
-            with custom CSS or overriding our default variables. It's also worth
-            noting that just about any HTML can go within the .accordion-body ,
-            though the transition does limit overflow.
-          </AccordionBody>
+          {Data.map((elment, index) => {
+            return (
+              <>
+                <AccordionHeader targetId={Number(index) + 1} className="khaled">
+                  <strong>{elment.qustion}</strong>
+                </AccordionHeader>
+                <AccordionBody accordionId={index + 1}>
+                  {elment.answer}
+                </AccordionBody>
+              </>
+            );
+          })}
         </AccordionItem>
       </UncontrolledAccordion>
     </div>
