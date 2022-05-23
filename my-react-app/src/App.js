bd339fc184284cdaeafb5d92b846e1f7689f6134
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-23 15:50:13 +0300

    add file for backend

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,18 +1,16 @@
-import './App.css';
+import "./App.css";
 import ReactDOM from "react-dom";
 import { Route, Routes } from "react-router-dom";
-import Navigation from "./component/Navbar/navbar"
-import Faqs from "./pages/FAQ/FAQs"
+import Navigation from "./component/Navbar/navbar";
+import Faqs from "./pages/FAQ/FAQs";
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
       <Routes>
-        <Route path="/FAQs" element={<Faqs/>} />
+        <Route path="/FAQs" element={<Faqs />} />
       </Routes>
       
-    
-
     </div>
   );
 }
