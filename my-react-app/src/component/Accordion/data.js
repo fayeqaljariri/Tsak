e3d593b7d5135608454b03d8bfa5090f9822faa8
fayeqaljariri <aljariri.fayeq@gmail.com>
2022-05-23 15:54:27 +0300

    add data file

---
+++ my-react-app/src/component/Accordion/data.js
@@ -0,0 +1,9 @@
+export const Data = [
+  { qustion: "lorem?", answer: "ssdkn ;sdvnls s;dj oweif ld" },
+  {
+    qustion: "Lorem hsidhs ?",
+    answer:
+      " This is the second item's accordion body. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the .accordion-body , though the transition does limit overflow.",
+  },
+  { qustion: "lorem?", answer: "ssdkn ;sdvnls s;dj oweif ld" },
+];
